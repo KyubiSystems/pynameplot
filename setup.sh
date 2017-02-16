e2e955245f2c9945c0123a4622ea4be1268b451f
Duncan Law-Green <dlg@kyubi.co.uk>
2017-02-16 17:52:29 +0000

    Tidy KMZ files, add Basemap support

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+module load python/gcc/27
 module load gdal
 module load geos
+. ./venv.geo/bin/activate
 export LD_LIBRARY_PATH="/cm/shared/apps/geos/3.6.1/lib:"$LD_LIBRARY_PATH
+export GEOS_DIR="/cm/shared/apps/geos/3.6.1/"
