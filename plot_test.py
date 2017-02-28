e43d6eeea33d9f1b3dd2b28dff2597c40e9a2f55
Duncan Law-Green <dlg@kyubi.co.uk>
2017-02-28 15:01:01 +0000

    Updating plotting params

--- plot_test.py
+++ plot_test.py
@@ -4,9 +4,16 @@ from mpl_toolkits.basemap import Basemap
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+
+title = '1daysurfaceWARRI_20110715.txt'
 
 lon_bounds = [ 2., 7. ]
 lat_bounds = [ 1., 8. ]
+
+lon_axis = [4., 6.]
+lat_axis = [2., 4., 6.]
 
 fig, ax = plt.subplots()
 
@@ -15,14 +22,16 @@ ax.set_aspect('equal')
 m = Basemap(llcrnrlon=lon_bounds[0], llcrnrlat=lat_bounds[0],
             urcrnrlon=lon_bounds[1], urcrnrlat=lat_bounds[1],
             projection='lcc', lat_1=2., lat_2=6., lon_0=4.,
-            resolution='l', area_thresh=1000.)
+            resolution='i', area_thresh=1000.)
 
-m.drawcoastlines()
-m.drawcountries()
-m.drawmapboundary(fill_color='#99ffff')
-m.fillcontinents(color='#cc9966',lake_color='#99ffff')
+m.drawcoastlines(color='white')
+m.drawcountries(color='white')
+m.drawmapboundary(fill_color='#444444')
+m.fillcontinents(color='#bbbbbb',lake_color='#444444')
+m.drawparallels(lat_axis, linewidth=0.5, color='white')
+m.drawmeridians(lon_axis, linewidth=0.5, color='white')
 
-ax.set_title('Test Basemap plot', fontsize=20)
+ax.set_title(title, fontsize=12)
 
 pngfile = 'plot_test.png'
 fig.savefig(pngfile, dpi=300)
