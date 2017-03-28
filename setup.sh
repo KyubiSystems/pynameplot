884e0f520cd898e08338c9c1695ffef00cce44d1
Duncan Law-Green <dlg@kyubi.co.uk>
2017-03-28 17:59:20 +0100

    OOP test of covering factor calc

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ module load geos
 . ./venv.geo/bin/activate
 export LD_LIBRARY_PATH="/cm/shared/apps/geos/3.6.1/lib:"$LD_LIBRARY_PATH
 export GEOS_DIR="/cm/shared/apps/geos/3.6.1/"
+export SPATIALINDEX_C_LIBRARY="/cm/shared/apps/spatialindex/1.8.5/lib/libspatialindex_c.so"
---
+++ testcf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# Author: Duncan Law-Green (dlg@kyubi.co.uk)
+# Copyright 2017 Kyubi Systems
+# 
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or in writing, software 
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from namereader import *
+
+# instantiate new NAME object
+name = name.Name('PML_NAME_output/low5dayPML_20150501.txt')
+
+# get extremal concentration values                                                                                                                                                  
+(min_conc, max_conc) = name.get_minmax()
+
+print "Minimum concentration: ", min_conc
+print "Maximum concentration: ", max_conc
+
+# Generate covering factor column for input ESRI shapefile                                                                                                                           
+print 'Starting covering factor calculation...',
+name.get_cover('Final_PML_shapefiles_15_03_17/England.shp')
+print ' done.'
+
+n = name.data['England']
+print n.loc[n != 0.0]
